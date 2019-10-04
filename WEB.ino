fe157d55c757d74f38825173c555f215f1a0ea83
Franz Hoffmann <Franz>
2019-10-04 16:46:38 +0200

    added some html

--- WEB.ino
+++ WEB.ino
@@ -1,46 +1,32 @@
 void setup_webserver(ESP8266WebServer *s) {
-  s->on("/",    handleRoot);
-  s->on("/cfg", handleConfig);
-  s->on("/sta", handleStatus);
-  s->on("/log", handleLog);
-  s->on("/s",   handleAjax);
+  s->on("/",     handleRoot);
+  s->on("/cfg",  handleConfig);
+  s->on("/wifi", handleWifi);
+  s->on("/log",  handleLog);
+  s->on("/s",    handleAjax);
   s->begin();
 }
 
 // ----------------------------------------------------------------------------- web server
 String getContentType(String filename) {
-  if (server.hasArg("download")) {
-    return "application/octet-stream";
-  } else if (filename.endsWith(".htm")) {
-    return "text/html";
-  } else if (filename.endsWith(".html")) {
-    return "text/html";
-  } else if (filename.endsWith(".css")) {
-    return "text/css";
-  } else if (filename.endsWith(".js")) {
-    return "application/javascript";
-  } else if (filename.endsWith(".png")) {
-    return "image/png";
-  } else if (filename.endsWith(".gif")) {
-    return "image/gif";
-  } else if (filename.endsWith(".jpg")) {
-    return "image/jpeg";
-  } else if (filename.endsWith(".ico")) {
-    return "image/x-icon";
-  } else if (filename.endsWith(".xml")) {
-    return "text/xml";
-  } else if (filename.endsWith(".pdf")) {
-    return "application/x-pdf";
-  } else if (filename.endsWith(".zip")) {
-    return "application/x-zip";
-  } else if (filename.endsWith(".gz")) {
-    return "application/x-gzip";
-  }
+  if (server.hasArg("download"))    return "application/octet-stream";
+  if (filename.endsWith(".htm"))    return "text/html";
+  if (filename.endsWith(".html"))   return "text/html";
+  if (filename.endsWith(".css"))    return "text/css";
+  if (filename.endsWith(".js"))     return "application/javascript";
+  if (filename.endsWith(".png"))    return "image/png";
+  if (filename.endsWith(".gif"))    return "image/gif";
+  if (filename.endsWith(".jpg"))    return "image/jpeg";
+  if (filename.endsWith(".ico"))    return "image/x-icon";
+  if (filename.endsWith(".xml"))    return "text/xml";
+  if (filename.endsWith(".pdf"))    return "application/x-pdf";
+  if (filename.endsWith(".zip"))    return "application/x-zip";
+  if (filename.endsWith(".gz"))     return "application/x-gzip";
   return "text/plain";
 }
 
 
-void returnFail(String msg) {
+void fail(String msg) {
   server.send(500, "text/plain", msg + "\r\n");
 }
 
@@ -53,8 +39,18 @@ String substitute(String var) {
   if (var == "KP") return String(p.kp);
   if (var == "TN") return String(p.tn);
   if (var == "TIME_LEFT") return String(p.set_time - p.act_time);
+  if (var == "HOSTNAME") return HOSTNAME;
+  if (var == "SSID") return p.ssid;
   
-  return var;
+  if (var == "CSS")  {
+    File f = filesystem->open("stylesheet.css", "r");
+    if (!f) return "";
+    String s = f.readString();
+    f.close();
+    return s;
+  }
+  
+  return var; // ignore unknown strings
 }
 
 /* 
@@ -64,11 +60,11 @@ String substitute(String var) {
  */
 void send_file(String filename) {
   if (!filesystem->exists(filename)) {
-    return returnFail("file not found: " + filename);
+    return fail("file not found: " + filename);
   }
   File f = filesystem->open(filename, "r");
   if (!f) {
-    return returnFail("could not open file");
+    return fail("could not open file");
   }
   
   String reply; reply.reserve(8192);
@@ -135,6 +131,6 @@ void send_file(String filename) {
 
 void handleRoot()   {send_file("/index.html");}
 void handleConfig() {send_file("/cfg.html");}
-void handleStatus() {send_file("/status.html");}
+void handleWifi()   {send_file("/wifi.html");}
 void handleLog()    {send_file("/log.html");}
 void handleAjax()   {send_file("/ajax.json");}
